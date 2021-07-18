65865eb7e836aea714f935f3720bce19b8a50009
Lianan <2779799659@qq.com>
2021-07-19 07:56:37 +0800

    新增回到顶部

--- src/ui/iconlist.js
+++ src/ui/iconlist.js
@@ -1,3 +1,15 @@
+import { BackTop } from 'antd';
+const style = {
+  height: 40,
+  width: 40,
+  lineHeight: '40px',
+  borderRadius: 4,
+  backgroundColor: '#1088e9',
+  color: '#fff',
+  textAlign: 'center',
+  fontSize: 14,
+};
+
 function Iconlist() {
   return (
     <div>
@@ -3650,6 +3662,9 @@ function Iconlist() {
           </li>
         </ul>
       </div>
+      <BackTop>
+        <div style={style}>UP</div>
+      </BackTop>
     </div>
   );
 }
